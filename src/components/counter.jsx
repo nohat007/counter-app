afc89dbd1328753ff53209636b2dc72965e41733
nohat yacinthe <nohatyacinthe1@hotmail.fr>
2019-04-05 20:09:32 -0400

    added some functonality

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
   render() {
-    return <h1>Hello World</h1>;
+    // i can put the classes in the curly braces or
+    // i can directly call the method on this
+    // let classes = this.getClass();
+
+    return (
+      <React.Fragment>
+        <span className={this.getClass()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  getClass() {
+    let classes = "badge m-2 ";
+    classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+    return classes;
+  }
+
+  formatCount() {
+    // this I destructured to but the value in count variable
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
