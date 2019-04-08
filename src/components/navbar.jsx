0b882a9c460f03c848cb8f680fe94c919927429b
nohat yacinthe <nohatyacinthe1@hotmail.fr>
2019-04-08 15:33:35 -0400

    done the tutorial now with my own stuff

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,23 @@
 import React, { Component } from "react";
 import Counters from "./counters";
 
+// stateless functional component
+//at runtime react passes props so i dont need the this
+const NavBar = props => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill bagde-secondary">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
 class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge badge-pill bagde-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
+  render() {}
 }
 
 export default NavBar;
